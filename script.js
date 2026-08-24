ad1796ff824970ce8aa90dbc6c164d7516943426
allanydias <allanydiasnave@gmail.com>
2026-08-24 16:55:28 -0300

    feat: enhance activity form and improve date formatting in task rendering

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+//biblioteca de terceiros - day.js
+const formate = data => {
+  return {
+    dia: {
+      numerico:dayjs(data).format('DD'),
+      semana: {
+        curto: dayjs(data).format('ddd'),
+        longo: dayjs(data).format('dddd'),
+      }
+    },
+    mes: dayjs(data.format('MMMM')),
+    hora: dayjs(data).format('HH:mm')
+  }
+}
+
+
 // objeto chave-valor por isso os dados nas {}. Diferente da função
 const tarefa = {
   name: 'Olhar os E-mails',
   data: new Date('2026-08-19 10:30'),
-  finalizada: true
-
+  // data tem que colocar aspas
+  finalizada: true //atributo
 };
-// data tem que colocar aspas
 
 //array == lista 
-const atividades = [
+let atividades = [
   tarefa, {
     name: 'Separar as roupas que eu não uso',
     data: new Date('2026-08-25  15:00'),
@@ -21,29 +36,44 @@ const atividades = [
   }
 ]
 
+atividades = []
+
 // chaves aqui entra como uma função = arrow fuction
-const criarItemDeAtividade = (tarefa) => {
+const criarItemDeAtividade = (tarefa) => { 
 
   let input = '<input type="checkbox" '
 
-  if(tarefa.finalizada) {
+  if(tarefa.finalizada ) {
     input += 'checked' //pega o imput antigo com a cocatenação do checked para ficar com o check
   }
 
   input += '>' //completar a linha 13
 
+  const formatar = formatador(atividade.data);
+
+
   return ` <section>
         <div>
           ${input}
           <span>${tarefa.name}</span> 
-          <time>${tarefa.data}</time>
+          <time>${formatar.dia.semana.longo}, 
+          dia ${formatar.dia.numerico} de ${formatar.mes} às ${formatar.hora}h </time>
         </div>
       </section>`; //templet literal, 🚩lembrar do ;
 };
 
-// interpolar é chaves e não conchete
-const section = document.querySelector('section');
+const atualizarListaDeAtividade = () => { // interpolar é chaves e não conchete
+  const section = document.querySelector('section');
 
-for(let atividade of atividades){
-  section.innerHTML += criarItemDeAtividade(atividade);
+  //verificar se a minha lista está vazia
+  if(atividades.length == 0) { //length significa quantos elementos tem na lista
+
+    section.innerHTML = `<p>Nenhuma Atividade cadastrada.</p>`
+    return //para a aplicação
+  } 
+  
+  for(let atividade of atividades){
+    section.innerHTML += criarItemDeAtividade(atividade);
+  }
+
 }
